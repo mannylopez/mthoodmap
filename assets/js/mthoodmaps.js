d5e4055ea0288213a58d482ed916fbf3a7ebaa73
manny <Manuel_Lopez@esri.com>
2014-03-31 15:05:27 -0700

    adding photo lat/long markers to map

--- assets/js/mthoodmaps.js
+++ assets/js/mthoodmaps.js
@@ -1,5 +1,7 @@
 var map = L.map('map').setView([45.395577, -121.653465], 13);
 
-L.esri.basemapLayer('Topographic').addTo(map);
+L.esri.basemapLayer('Imagery').addTo(map);
 
 omnivore.gpx('/assets/data/hike.gpx').addTo(map);
+
+omnivore.csv('assets/data/onlyPhotoLatLong.csv').addTo(map);
