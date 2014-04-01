5a2ad2117335a8659aa607118f5adc4f7da7ea16
manny <Manuel_Lopez@esri.com>
2014-03-31 18:14:38 -0700

    adding pop up and photos

--- assets/js/mthoodmaps.js
+++ assets/js/mthoodmaps.js
@@ -2,6 +2,18 @@ var map = L.map('map').setView([45.395577, -121.653465], 13);
 
 L.esri.basemapLayer('Imagery').addTo(map);
 
-omnivore.gpx('/assets/data/hike.gpx').addTo(map);
+var trailFeatures = omnivore.gpx('assets/data/hike.gpx').addTo(map);
 
-omnivore.csv('assets/data/onlyPhotoLatLong.csv').addTo(map);
+var photoFeatures = omnivore.csv('assets/data/photoLatLong.csv');
+
+photoFeatures.on('ready', function() {
+  photoFeatures.eachLayer(function(layer) {
+    // console.log(layer.feature.properties.SourceFile);
+    layer.bindPopup('<img class="mthoodimage" src="assets/photos/' + layer.feature.properties.SourceFile + '">');
+
+  });
+}).addTo(map);
+
+//gpx.on('click', function() { alert('Clicked on a group!'); })
+
+//photoFeatures.on('click', function(e) {console.log('Clicked on a group!', e.target); })
